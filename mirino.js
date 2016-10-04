3ee9abafaff020910bdf79340f4ae29707595b04
Trevor Pellegrini <trevorpellegrini@gmail.com>
2016-10-04 13:28:07 -0700

    Dom step one

--- mirino.js
+++ mirino.js
@@ -1,134 +1,134 @@
 "use strict"
-function initialize() {
-var placesLocation = [];
-var lat = 37.79;
-var lag =  -122.4;
-var photoInfo = {
-  picId: [],
-  farmId: [],
-  serverId: [],
-  secretId: [],
-  tag: []
-};
-var photoURL = [];
-var pyrmont = new google.maps.LatLng(lat, lag);
+window.onload = function(){
+  initialize();
+  function initialize() {
+  var placesLocation = [];
+  var lat = 37.79;
+  var lag =  -122.4;
+  var photoInfo = {
+    picId: [],
+    farmId: [],
+    serverId: [],
+    secretId: [],
+    tag: []
+  };
+  var photoURL = [];
+  var pyrmont = new google.maps.LatLng(lat, lag);
 
-var map = new google.maps.Map(document.getElementById('map'), {
-center: pyrmont,
-zoom: 15,
-scrollwheel: false
-});
-
-// Specify location, radius and place types for your Places API search.
-var request = {
-location: pyrmont,
-radius: '500',
-types: ['store']
-};
-
-// Create the PlaceService and send the request.
-// Handle the callback with an anonymous function.
-// var service = new google.maps.places.PlacesService(map);
-// service.nearbySearch(request, function(results, status) {
-// if (status == google.maps.places.PlacesServiceStatus.OK) {
-//   for (var i = 0; i < results.length; i++) {
-//     var place = results[i];
-//     // If the request succeeds, draw the place location on
-//     // the map as a marker, and register an event to handle a
-//     // click on the marker.
-//     var marker = new google.maps.Marker({
-//       map: map,
-//       position: place.geometry.location
-//     });
-//   }
-// }
-// });
-
-//listens for clicks on the map div;
-google.maps.event.addListener(map,'click',function(e){
-  console.log('clicked @'+e.latLng);
-  var lat = e.latLng.lat();
-  var lng = e.latLng.lng();
-  console.log(lat);
-  console.log(lng);
-  $.ajax({
-    url:`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0bf0839ee1fe6ee109720782d7ec8a63&safe_search=1&has_geo=true&lat=${lat}&lon=${lng}&radius=1&accuracy=11&tags=giants&per_page=10&format=json&nojsoncallback=1`,
-    method: "GET",
-    success: function(data){
-      for(let i = 0; i < data.photos.photo.length; i++){
-        let path = data.photos.photo[i];
-          if(photoInfo.picId.indexOf(data.photos.photo[i].id) === -1 &&   path.id !== undefined && path.farm !== undefined && path.server !== undefined && path.secret !== undefined){
-              photoInfo.picId[i] = path.id;
-              photoInfo.farmId[i] = path.farm;
-              photoInfo.serverId[i] = path.server;
-              photoInfo.secretId[i] = path.secret;
-              photoURL.push(`http://farm${photoInfo.farmId[i]}.staticflickr.com/${photoInfo.serverId[i]}/${photoInfo.picId[i]}_${photoInfo.secretId[i]}.jpg`);
-          // for(let i = 0; i <= photoInfo.picId.length;i++){
-            //http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-
-            //  $.ajax({
-            //    url:`https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=0bf0839ee1fe6ee109720782d7ec8a63&photo_id=${photoInfo.picId[i]}&format=json&nojsoncallback=1`,
-            //    method: "GET",
-            //    success: function(data){
-            //      if(photoURL.indexOf(data.photo.urls.url[0]._content) === -1){
-            //        photoURL.push(data.photo.urls.url[0]._content);
-            //      }
-            //    }
-            //  });
-           //}
-        }
-      }
-      console.log(photoInfo);
-      console.log(photoURL);
-    }
+  var map = new google.maps.Map(document.getElementById('map'), {
+  center: pyrmont,
+  zoom: 15,
+  scrollwheel: false
   });
-  var main = document.getElementById("container");
-  for(var i = 0; i < photoURL; i++){
-    var img = document.createElement("img");
-    var currentImg = img.setAttribute('src', photoURL[i]);
-    $(main).append(currentImg);
-}
-    //  for(let i = 0; i <= photoID.length;i++){
-    //     $.ajax({
-    //       url:`https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=0bf0839ee1fe6ee109720782d7ec8a63&photo_id=${photoID[i]}&format=json&nojsoncallback=1`,
-    //       method: "GET",
-    //       success: function(data){
-    //         if(photoURL.indexOf(data.photo.urls.url[0]._content) === -1){
-    //           photoURL.push(data.photo.urls.url[0]._content);
-    //         }
-    //       }
-    //     });
-    //   }
-    //   console.log(photoURL);
 
-  // var loc = location.latLag;
-  // var request = {
-  //   location: e.latLng,
-  //   radius: '500',
-  //   types: ['store']
-  // };
-  //
-  // // Create the PlaceService and send the request.
-  // // Handle the callback with an anonymous function.
+  // Specify location, radius and place types for your Places API search.
+  var request = {
+  location: pyrmont,
+  radius: '500',
+  types: ['store']
+  };
+
+  // Create the PlaceService and send the request.
+  // Handle the callback with an anonymous function.
   // var service = new google.maps.places.PlacesService(map);
   // service.nearbySearch(request, function(results, status) {
-  //   var placeHolder = [];
-  //   if (status == google.maps.places.PlacesServiceStatus.OK) {
-  //     for (var i = 0; i < results.length; i++) {
-  //       var place = results[i];
-  //       // If the request succeeds, draw the place location on
-  //       // the map as a marker, and register an event to handle a
-  //       // click on the marker.
-  //       placeHolder.push(place);
-  //
-  //       var marker = new google.maps.Marker({
-  //         map: map,
-  //         position: place.geometry.location
-  //       });
-  //     }
-  //     console.log(placeHolder);
+  // if (status == google.maps.places.PlacesServiceStatus.OK) {
+  //   for (var i = 0; i < results.length; i++) {
+  //     var place = results[i];
+  //     // If the request succeeds, draw the place location on
+  //     // the map as a marker, and register an event to handle a
+  //     // click on the marker.
+  //     var marker = new google.maps.Marker({
+  //       map: map,
+  //       position: place.geometry.location
+  //     });
   //   }
+  // }
   // });
-});
-}
+
+  //listens for clicks on the map div;
+  google.maps.event.addListener(map,'click',function(e){
+    console.log('clicked @'+e.latLng);
+    var lat = e.latLng.lat();
+    var lng = e.latLng.lng();
+    var button = document.getElementById("buttonAnchor");
+    button.setAttribute("href",`mirinoPhotos.html?lat=${lat}&lng=${lng}`);
+    console.log(lat);
+    console.log(lng);
+    // $.ajax({
+    //   url:`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0bf0839ee1fe6ee109720782d7ec8a63&safe_search=1&has_geo=true&lat=${lat}&lon=${lng}&radius=1&accuracy=11&tags=giants&per_page=10&format=json&nojsoncallback=1`,
+    //   method: "GET",
+    //   success: function(data){
+    //     for(let i = 0; i < data.photos.photo.length; i++){
+    //       let path = data.photos.photo[i];
+    //         if(photoInfo.picId.indexOf(data.photos.photo[i].id) === -1 &&   path.id !== undefined && path.farm !== undefined && path.server !== undefined && path.secret !== undefined){
+    //             photoInfo.picId[i] = path.id;
+    //             photoInfo.farmId[i] = path.farm;
+    //             photoInfo.serverId[i] = path.server;
+    //             photoInfo.secretId[i] = path.secret;
+    //             photoURL.push(`http://farm${photoInfo.farmId[i]}.staticflickr.com/${photoInfo.serverId[i]}/${photoInfo.picId[i]}_${photoInfo.secretId[i]}.jpg`);
+    //         // for(let i = 0; i <= photoInfo.picId.length;i++){
+    //           //http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+    //
+    //           //  $.ajax({
+    //           //    url:`https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=0bf0839ee1fe6ee109720782d7ec8a63&photo_id=${photoInfo.picId[i]}&format=json&nojsoncallback=1`,
+    //           //    method: "GET",
+    //           //    success: function(data){
+    //           //      if(photoURL.indexOf(data.photo.urls.url[0]._content) === -1){
+    //           //        photoURL.push(data.photo.urls.url[0]._content);
+    //           //      }
+    //           //    }
+    //           //  });
+    //          //}
+    //       }
+    //     }
+    //     console.log(photoInfo);
+    //     console.log(photoURL);
+    //   }
+    // });
+      //  for(let i = 0; i <= photoID.length;i++){
+      //     $.ajax({
+      //       url:`https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=0bf0839ee1fe6ee109720782d7ec8a63&photo_id=${photoID[i]}&format=json&nojsoncallback=1`,
+      //       method: "GET",
+      //       success: function(data){
+      //         if(photoURL.indexOf(data.photo.urls.url[0]._content) === -1){
+      //           photoURL.push(data.photo.urls.url[0]._content);
+      //         }
+      //       }
+      //     });
+      //   }
+      //   console.log(photoURL);
+
+    // var loc = location.latLag;
+    // var request = {
+    //   location: e.latLng,
+    //   radius: '500',
+    //   types: ['store']
+    // };
+    //
+    // // Create the PlaceService and send the request.
+    // // Handle the callback with an anonymous function.
+    // var service = new google.maps.places.PlacesService(map);
+    // service.nearbySearch(request, function(results, status) {
+    //   var placeHolder = [];
+    //   if (status == google.maps.places.PlacesServiceStatus.OK) {
+    //     for (var i = 0; i < results.length; i++) {
+    //       var place = results[i];
+    //       // If the request succeeds, draw the place location on
+    //       // the map as a marker, and register an event to handle a
+    //       // click on the marker.
+    //       placeHolder.push(place);
+    //
+    //       var marker = new google.maps.Marker({
+    //         map: map,
+    //         position: place.geometry.location
+    //       });
+    //     }
+    //     console.log(placeHolder);
+    //   }
+    // });
+  });
+
+  }
+};
 //google.maps.event.addDomListener(window, 'load', initialize);
---
+++ mirinoLoadPhoto.js
@@ -0,0 +1,120 @@
+"use strict"
+var placesLocation = [];
+var photoInfo = {
+  picId: [],
+  farmId: [],
+  serverId: [],
+  secretId: [],
+  tag: [],
+  country: [],
+  county: [],
+  imgURL: []
+};
+  var photoURL = [];
+
+  readQueryParams();
+
+  function readQueryParams() {
+
+      if(document.location.search) {
+          var queryString = document.location.search.replace('?', '');
+
+          queryString.split('&').forEach(function (pair) {
+              var pairArray = pair.split('=');
+              placesLocation.push(pairArray[1]);
+          });
+      }
+      console.log(placesLocation);
+  }
+
+$.ajax({
+  url:`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0bf0839ee1fe6ee109720782d7ec8a63&safe_search=1&has_geo=true&lat=${placesLocation[0]}&lon=${placesLocation[1]}&radius=1&accuracy=11&tags=giants&per_page=10&format=json&nojsoncallback=1`,
+  method: "GET",
+  success: function(data){
+    for(let i = 0; i < data.photos.photo.length; i++){
+      let path = data.photos.photo[i];
+        if(photoInfo.picId.indexOf(data.photos.photo[i].id) === -1 &&   path.id !== undefined && path.farm !== undefined && path.server !== undefined && path.secret !== undefined){
+            photoInfo.picId[i] = path.id;
+            photoInfo.farmId[i] = path.farm;
+            photoInfo.serverId[i] = path.server;
+            photoInfo.secretId[i] = path.secret;
+            photoURL.push(`http://farm${photoInfo.farmId[i]}.staticflickr.com/${photoInfo.serverId[i]}/${photoInfo.picId[i]}_${photoInfo.secretId[i]}.jpg`);
+           $.ajax({
+             url:`https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=0bf0839ee1fe6ee109720782d7ec8a63&photo_id=${photoInfo.picId[i]}&format=json&nojsoncallback=1`,
+             method: "GET",
+             success: function(data){
+              photoInfo.country[i] =
+              data.photo.location.country._content;
+              photoInfo.county[i] = data.photo.location.county._content;
+              photoInfo.imgURL[i] = data.photo.urls.url[0]._content;
+              createDom()
+
+              function createDom(){
+              var main = document.getElementById("container");
+                //for(let i = 0; i < photoURL.length; i++){
+                  let row = document.createElement("div");
+                    row.className = "row";
+                  let col = document.createElement("div");
+                    col.className = "col s12 m7";
+                  let card = document.createElement("div");
+                    card.className = "card";
+                  let cardImg = document.createElement("div");
+                    cardImg.className = "card-image";
+                  let img = document.createElement("img");
+                      img.setAttribute('src', photoURL[i]);
+                  let content = document.createElement("div");
+                      content.className = "card-content";
+                  let paragraph = document.createElement("p");
+                      paragraph.innerText = photoInfo.county[i];
+                  let action = document.createElement("div");
+                      action.className = "card-action";
+                  let anchor = document.createElement("a");
+                      anchor.setAttribute("href", photoInfo.imgURL[i]);
+                      anchor.innerText = "link to image"
+
+                  var appendToCard = main.appendChild(row).appendChild(col).appendChild(card);
+
+                  appendToCard.appendChild(cardImg).appendChild(img);
+                  appendToCard.appendChild(content).appendChild(paragraph);
+                  appendToCard.appendChild(action).appendChild(anchor);
+
+                //}
+              }
+             }
+          });
+        }
+      }
+      //   var main = document.getElementById("container");
+      //   for(var i = 0; i < photoURL.length; i++){
+      //     let row = $("<div class = 'row'>");
+      //     let col = $("<div class = 'col s12 m7'>");
+      //     let card = $("<div class = 'card'>");
+      //     let cardImg = $("<div class = 'card-image'>");
+      //     let img = $(`<img src = ${photoURL[i]}>`);
+      //     let content =  $("<div class='card-content'>");
+      //     let para = $("<p>");
+      //      $(para).text("Hello");
+      //     //let currentImg = img.setAttribute('src', photoURL[i]);
+      //     $(main).append(row).append(col).append(card).append(cardImg).append(img).append(content).append(para);
+      // }
+    //   <div class="row">
+    //    <div class="col s12 m7">
+    //      <div class="card">
+    //        <div class="card-image">
+    //          <img src="http://farm8.staticflickr.com/7526/29809612290_d516f241d5.jpg">
+    //          <span class="card-title">Card Title</span>
+    //        </div>
+    //        <div class="card-content">
+    //          <p>I am a very simple card. I am good at containing small bits of information.
+    //          I am convenient because I require little markup to use effectively.</p>
+    //        </div>
+    //        <div class="card-action">
+    //          <a href="#">This is a link</a>
+    //        </div>
+    //      </div>
+    //    </div>
+    //  </div>
+      console.log(photoInfo);
+      console.log(photoURL);
+    }
+  });

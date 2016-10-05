71b94f3ace088a78bf6af5c457ae79a0e55c5dc2
Trevor Pellegrini <trevorpellegrini@gmail.com>
2016-10-04 20:59:33 -0700

    thanks tyler

--- mirinoLoadPhoto.js
+++ mirinoLoadPhoto.js
@@ -1,4 +1,7 @@
 "use strict"
+$(document).ready(function(){
+  $(".button-collapse").sideNav();
+});
 var placesLocation = [];
 var photoInfo = {
   picId: [],
@@ -41,11 +44,12 @@ $.ajax({
     // for loop to create rows
     let main = document.getElementById("container");
       // for loop to create columns
-
+    var currentRow;
     for(let i = 0; i < data.photos.photo.length; i++){
       if(i % 3 === 0){
-        var rows = document.createElement("div");
-        rows.className = "row col m12";
+        currentRow = document.createElement("div");
+        main.appendChild(currentRow);
+        currentRow.className = "row col m12";
         console.log("hello");
       }
       let path = data.photos.photo[i];
@@ -55,63 +59,11 @@ $.ajax({
             photoInfo.serverId[i] = path.server;
             photoInfo.secretId[i] = path.secret;
             photoURL.push(`http://farm${photoInfo.farmId[i]}.staticflickr.com/${photoInfo.serverId[i]}/${photoInfo.picId[i]}_${photoInfo.secretId[i]}.jpg`);
+
            $.ajax({
              url:`https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=0bf0839ee1fe6ee109720782d7ec8a63&photo_id=${photoInfo.picId[i]}&format=json&nojsoncallback=1`,
              method: "GET",
-             success: function(data){
-              photoInfo.country[i] =
-              data.photo.location.country._content;
-              var tagNames = [];
-              for(let tagNumber = 0; tagNumber < 5; tagNumber++){
-                tagNames.push(data.photo.tags.tag[tagNumber].raw);
-              }
-              console.log(tagNames);
-              photoInfo.tag[i] = tagNames;
-              console.log(data.photo.tags.tag[0].raw);
-              photoInfo.county[i] = data.photo.location.county._content;
-              photoInfo.imgURL[i] = data.photo.urls.url[0]._content;
-              //let main = document.getElementById("container");
-              //let rows = document.createElement("div");
-                  //rows.className = "row col m12";
-                  // rows.className = "rows row col m12";
-                  // main.appendChild(rows);
-                  main.appendChild(rows).appendChild(createDom());
-
-              function createDom(){
-                //for(let i = 0; i < photoURL.length; i++){
-                  let row = document.createElement("div");
-                    row.className = "row";
-                  let col = document.createElement("div");
-                    col.className = "col s12 m7";
-                  let card = document.createElement("div");
-                    card.className = "card";
-                  let cardImg = document.createElement("div");
-                    cardImg.className = "card-image";
-                  let img = document.createElement("img");
-                      img.setAttribute('src', photoURL[i]);
-                  let content = document.createElement("div");
-                      content.className = "card-content";
-                  let paragraph = document.createElement("p");
-                      paragraph.innerText = "#" +  photoInfo.tag[i].join("#");
-                  let paragraph1 = document.createElement("p");
-                      paragraph1.innerText = photoInfo.county[i];
-                  let action = document.createElement("div");
-                      action.className = "card-action";
-                  let anchor = document.createElement("a");
-                      anchor.setAttribute("href", photoInfo.imgURL[i]);
-                      anchor.innerText = "link to image";
-
-                  var appendToCard =
-                  (row).appendChild(col).appendChild(card);
-
-                  appendToCard.appendChild(cardImg).appendChild(img);
-                  appendToCard.appendChild(content).appendChild(paragraph1).appendChild(paragraph);
-                  appendToCard.appendChild(action).appendChild(anchor);
-
-                  return row;
-                //}
-              }
-             }
+             success: generateCurrentRowCB(currentRow, i)
           });
         }
       }
@@ -150,3 +102,61 @@ $.ajax({
       console.log(photoURL);
     }
   });
+
+
+function generateCurrentRowCB(currentRow, i) {
+  return function(data) {
+   photoInfo.country[i] =
+   data.photo.location.country._content;
+   var tagNames = [];
+   for(let tagNumber = 0; tagNumber < 5; tagNumber++){
+     tagNames.push(data.photo.tags.tag[tagNumber].raw);
+   }
+   console.log(tagNames);
+   photoInfo.tag[i] = tagNames;
+   console.log(data.photo.tags.tag[0].raw);
+   photoInfo.county[i] = data.photo.location.county._content;
+   photoInfo.imgURL[i] = data.photo.urls.url[0]._content;
+   //let main = document.getElementById("container");
+   //let rows = document.createElement("div");
+       //rows.className = "row col m12";
+       // rows.className = "rows row col m12";
+       // main.appendChild(rows);
+   currentRow.appendChild(createDom(i));
+  }
+}
+
+function createDom(i){
+  //for(let i = 0; i < photoURL.length; i++){
+    let row = document.createElement("div");
+      row.className = "row";
+    let col = document.createElement("div");
+      col.className = "col s12 m7";
+    let card = document.createElement("div");
+      card.className = "card";
+    let cardImg = document.createElement("div");
+      cardImg.className = "card-image";
+    let img = document.createElement("img");
+        img.setAttribute('src', photoURL[i]);
+    let content = document.createElement("div");
+        content.className = "card-content";
+    let paragraph = document.createElement("p");
+        paragraph.innerText = "#" +  photoInfo.tag[i].join("#");
+    let paragraph1 = document.createElement("p");
+        paragraph1.innerText = photoInfo.county[i];
+    let action = document.createElement("div");
+        action.className = "card-action";
+    let anchor = document.createElement("a");
+        anchor.setAttribute("href", photoInfo.imgURL[i]);
+        anchor.innerText = "link to image";
+
+    var appendToCard =
+    (row).appendChild(col).appendChild(card);
+
+    appendToCard.appendChild(cardImg).appendChild(img);
+    appendToCard.appendChild(content).appendChild(paragraph1).appendChild(paragraph);
+    appendToCard.appendChild(action).appendChild(anchor);
+
+    return row;
+  //}
+}
